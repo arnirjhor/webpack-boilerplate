2bc35ec2a0a82c3fa7f8268de1847a3c5fb890df
postNirjhor <nirjhor.123@gmail.com>
2017-10-03 14:51:54 +0600

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,2 @@
+import './assets/scss/app.scss';
+console.log('Its working just fine');
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path'),
+      webpack = require('webpack'),
+      CleanWebpackPlugin = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({ filename: './assets/css/app.css' });
+
+const config = {
+
+  // absolute path for project root
+  context: path.resolve(__dirname, 'src'),
+
+  entry: {
+    // relative path declaration
+    app: './app.js'
+  },
+
+  output: {
+    // absolute path declaration
+    path: path.resolve(__dirname, 'dist'),
+    filename: './assets/js/[name].bundle.js'
+  },
+
+  module: {
+    rules: [
+
+      // babel-loader with 'env' preset
+      { test: /\.js$/, include: /src/, exclude: /node_modules/, use: { loader: "babel-loader", options: { presets: ['env'] } } },
+      // html-loader
+      { test: /\.html$/, use: ['html-loader'] },
+      // sass-loader with sourceMap activated
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      // file-loader(for images)
+      { test: /\.(jpg|png|gif|svg)$/, use: [ { loader: 'file-loader', options: { name: '[name].[ext]', outputPath: './assets/media/' } } ] },
+      // file-loader(for fonts)
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+
+    ]
+  },
+
+  plugins: [
+    // cleaning up only 'dist' folder
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    // extract-text-webpack-plugin instance
+    extractPlugin
+  ],
+
+  devServer: {
+    // static files served from here
+    contentBase: path.resolve(__dirname, "./dist/assets/media"),
+    compress: true,
+    // open app in localhost:2000
+    port: 2000,
+    stats: 'errors-only',
+    open: true
+  },
+
+  devtool: 'inline-source-map'
+
+};
+
+module.exports = config;
